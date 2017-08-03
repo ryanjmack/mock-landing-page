aa5a6a798f33a4104ddc7504468f68db3b63c30d
Ryan Mack <ryanjmack21@gmail.com>
2017-08-03 14:41:23 -0400

    Update nav text color on scroll

---
+++ src/js/general.js
@@ -0,0 +1,18 @@
+var navLinks = document.getElementById("nav-links");
+var menuButton = document.getElementById("menu-button");
+var nav = document.getElementById("nav");
+
+menuButton.addEventListener("click", function() {
+  navLinks.classList.toggle("mobile-nav");
+}, false);
+
+window.addEventListener("scroll", function() {
+  if (document.body.scrollTop > 0) {
+    nav.classList.add("scroll-down");
+    navLinks.classList.add("text-dark");
+  }
+  else {
+    nav.classList.remove("scroll-down");
+    navLinks.classList.remove("text-dark");
+  }
+});
