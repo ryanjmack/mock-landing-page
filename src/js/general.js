ff4aa36c12335359390c064f68df3376991c1464
Ryan Mack <ryanjmack21@gmail.com>
2017-08-22 14:19:12 -0400

    Add event listener to collapse mobile nav when nav link is clicked

--- src/js/general.js
+++ src/js/general.js
@@ -1,18 +1,23 @@
-var navLinks = document.getElementById("nav-links");
-var menuButton = document.getElementById("menu-button");
-var nav = document.getElementById("nav");
+(function() {
+    var navLinks = document.getElementById("nav-links");
+    var menuButton = document.getElementById("menu-button");
+    var nav = document.getElementById("nav");
 
-menuButton.addEventListener("click", function() {
-  navLinks.classList.toggle("mobile-nav");
-}, false);
+    menuButton.addEventListener("click", function() {
+        navLinks.classList.toggle("mobile-nav");
+    }, false);
 
-window.addEventListener("scroll", function() {
-  if (document.body.scrollTop > 0) {
-    nav.classList.add("scroll-down");
-    navLinks.classList.add("text-dark");
-  }
-  else {
-    nav.classList.remove("scroll-down");
-    navLinks.classList.remove("text-dark");
-  }
-});
+    navLinks.addEventListener("click", function() {
+        navLinks.classList.toggle("mobile-nav");
+    }, false);
+
+    window.addEventListener("scroll", function() {
+        if (document.body.scrollTop > 0) {
+            nav.classList.add("scroll-down");
+            navLinks.classList.add("text-dark");
+        } else {
+            nav.classList.remove("scroll-down");
+            navLinks.classList.remove("text-dark");
+        }
+    });
+}());
